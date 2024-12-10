c3d839438150ae43842b9a3b4ad32a69f1aaf1ec
Pancham Mehta <pancham1305@gmail.com>
2024-12-10 15:22:54 +0530

    Checked on Testcases

--- main.java
+++ main.java
@@ -6,5 +6,11 @@ public class main {
         Scanner sc = new Scanner(System.in);
         Max t = new Max(sc);
         System.out.println("The Maximum number is: " + t.getMax());
+        // Test Case 1: 9, 5, 3
+        // Output: 9
+        // Test Case 2: 3, 10, 2
+        // Output: 10
+        // Test Case 3: 4, 7, 12
+        // Output: 12
     }
 }
