a1e2032b959c6c485d8741b49281f105ea8ddafa
Pancham Mehta <pancham1305@gmail.com>
2024-12-10 15:31:13 +0530

    Added functionality for float numbers

--- main.java
+++ main.java
@@ -4,13 +4,11 @@ public class main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Max t = new Max(sc);
-        System.out.println("The Maximum number is: " + t.getMax());
-        // Test Case 1: 9, 5, 3
-        // Output: 9
-        // Test Case 2: 3, 10, 2
-        // Output: 10
-        // Test Case 3: 4, 7, 12
-        // Output: 12
+        Max t = new Max();
+        t.inputMaxInt(sc);
+        System.out.println("The Maximum number is: " + t.getMaxInteger());
+        t.inputMaxFloat(sc);
+        System.out.println("The Maximum number is: " + t.getMaxFloat());
+
     }
 }
