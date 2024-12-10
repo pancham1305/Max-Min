14dbe9eb25d2a75ff9bfe1076389c512b1fdaacd
Pancham Mehta <pancham1305@gmail.com>
2024-12-10 15:19:19 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Max t = new Max(sc);
+        System.out.println("The Maximum number is: " + t.getMax());
+    }
+}
